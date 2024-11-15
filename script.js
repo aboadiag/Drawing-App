ed39f8d2df2cd49a55aa51d8565c3d8eddb664bb
aboadiag <136507019+aboadiag@users.noreply.github.com>
2024-11-15 17:22:11 -0500

    misty test3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function(){
-    const BASE_URL = "https://72b0-35-245-28-142.ngrok-free.app"; // changed? dynamic public url! CHANGE EVERYTIME NGROK RESTARTS!
+    const BASE_URL = "https://6957-35-245-28-142.ngrok-free.app"; // changed? dynamic public url! CHANGE EVERYTIME NGROK RESTARTS!
     var paint=false;
     var paint_erase="paint";
     var canvas=document.getElementById("paint");
