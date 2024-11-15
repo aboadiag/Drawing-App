740d6e29d518d25da232685644d46c52222cb11b
aboadiag <136507019+aboadiag@users.noreply.github.com>
2024-11-15 15:43:57 -0500

    forgot the new url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(function(){
-    const BASE_URL = "https://2f83-35-245-28-142.ngrok-free.app"; // changed? dynamic public url! CHANGE EVERYTIME NGROK RESTARTS!
+    const BASE_URL = "https://ec8c-35-245-28-142.ngrok-free.app"; // changed? dynamic public url! CHANGE EVERYTIME NGROK RESTARTS!
     var paint=false;
     var paint_erase="paint";
     var canvas=document.getElementById("paint");
